5701c46f8b42d26d7cf2192836f1047f222a675f
Roland Gritzer <gritzer.roland@gmail.com>
2026-03-31 15:34:22 +0200

    extended thumbs, update config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,7 +8,7 @@
 #define MIRYOKU_NAV_VI
 /* #define MIRYOKU_CLIPBOARD_WIN */
 /* #define MIRYOKU_LAYERS_FLIP */
-/* #define MIRYOKU_MAPPING_EXTENDED_THUMBS */
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
 
 
 #define XXX &none
@@ -23,7 +23,7 @@
 XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
 XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
 XXX  K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX  \
-               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX XXX
+               XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 43 44
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 45 46
 #else
